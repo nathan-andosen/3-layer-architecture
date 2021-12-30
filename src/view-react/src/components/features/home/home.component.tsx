5064d2d66c56c51207a5a1d8ac1726c740d43e05
Nathan Anderson <nathan.anderson@acer.org>
2021-12-30 11:20:19 +1100

    More work on React

--- src/view-react/src/components/features/home/home.component.tsx
+++ src/view-react/src/components/features/home/home.component.tsx
@@ -6,19 +6,27 @@ import "./home.component.scss";
 import { DI } from '@thenja/di';
 import { UserService } from '@app-domain/services/user';
 
+interface IState {
+  wishListItem: string;
+};
 
 interface IProps extends RouteComponentProps {
 
 }
 
-class HomeComponent extends React.Component<IProps, undefined> {
+class HomeComponent extends React.Component<IProps, IState> {
   @DI.Inject(UserService)
   private userSrv: UserService;
+
+  private inputRef: React.RefObject<HTMLElement>;
 
 
   constructor(props: any) {
     super(props);
-
+    this.state = {
+      wishListItem: ''    
+    };
+    this.inputRef = React.createRef();
   }
 
   async signout() {
@@ -28,6 +36,21 @@ class HomeComponent extends React.Component<IProps, undefined> {
     });
   }
 
+  componentDidMount(): void {
+    // react does not work with custom events and web components, so we have
+    // to bind to events this way, using refs
+    this.inputRef.current.addEventListener('input', (event) => {
+      this.setState({wishListItem: event.target['value']});
+    });
+  }
+
+
+  async addItem() {
+
+    console.log('addItem()...', this.state);
+  }
+
+
   public render() {
     return (
       <div className="Home-component">
@@ -36,6 +59,19 @@ class HomeComponent extends React.Component<IProps, undefined> {
           <ui5-button onClick={(event) => this.signout()}>Sign out</ui5-button>
         </header>
         
+        <div className="add-to-wish-list-container">
+          <ui5-label for="myInput">Add to wish list:</ui5-label>
+          <form onKeyDown={(event) => {
+            if (event.keyCode === 13) this.addItem();
+          }} >
+            <ui5-input value={this.state.wishListItem}
+            ref={this.inputRef} required></ui5-input>
+            <ui5-button icon="add" onClick={(event) => this.addItem()} 
+            design="Emphasized">
+              Add
+            </ui5-button>
+          </form>
+        </div>
       </div>
     );
   }
--- src/view-react/src/typings/ui5.d.ts
+++ src/view-react/src/typings/ui5.d.ts
@@ -1,8 +1,16 @@
-// import * as React from 'react';
+
+// type Ui5Element = React
+//   .DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+
+type Ui5Element = any;
 
 declare namespace JSX {
   interface IntrinsicElements {
-    // "ui5-button": any;
-      "ui5-button": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "ui5-button": Ui5Element;
+    "ui5-label": Ui5Element;
+    "ui5-input": Ui5Element;
+    "ui5-message-strip": Ui5Element;
+    "ui5-list": Ui5Element;
+    "ui5-li": Ui5Element;
   }
 }
